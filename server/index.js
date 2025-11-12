cba03a38b3268b22e7f240b665fae9f8392aa1e1
Priyanshu <154051457+Priyanshu8433@users.noreply.github.com>
2025-11-12 22:16:47 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json({
+    status: 'success',
+    message: 'Welcome to the API',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on Port: ${port}`);
+});

f6440abbe3183864ab89a4c15351efcdef77392b
Priyanshu <154051457+Priyanshu8433@users.noreply.github.com>
2025-11-14 12:50:22 +0530

    basic next routing

---
+++ client/src/app/admin/dashboard/applications/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      applications
+    </div>
+  )
+}
+
+export default page
---
+++ client/src/app/admin/dashboard/hostels/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      hostels
+    </div>
+  )
+}
+
+export default page
